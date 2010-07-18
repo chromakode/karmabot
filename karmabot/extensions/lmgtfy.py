4c9021793d4143ce33bb244b9fd12717dc632507
Dan <dcolish@gmail.com>
2010-07-17 20:16:01 -0700

    fix unichr escaping

--- karmabot/extensions/lmgtfy.py
+++ karmabot/extensions/lmgtfy.py
@@ -1,13 +1,48 @@
 # dedicated to LC
 
 from json import JSONDecoder
-from urllib import urlencode
+from urllib import unquote, urlencode
 from urllib2 import urlopen
 
 from karmabot.core.client import thing
 from karmabot.core.commands.sets import CommandSet
 from karmabot.core.register import facet_registry
 from karmabot.core.thing import ThingFacet
+
+import re
+import htmlentitydefs
+
+##
+# Function Placed in public domain by Fredrik Lundh
+# http://effbot.org/zone/copyright.htm
+# http://effbot.org/zone/re-sub.htm#unescape-html
+# Removes HTML or XML character references and entities from a text string.
+#
+# @param text The HTML (or XML) source text.
+# @return The plain text, as a Unicode string, if necessary.
+
+
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        # leave as is
+        return text
+    return re.sub("&#?\w+;", fixup, text)
 
 
 @facet_registry.register
@@ -29,7 +64,7 @@ class LmgtfyFacet(ThingFacet):
         top_result = {}
         if response.get('responseStatus') == 200:
             results = response.get('responseData').get('results')
-            top_result = results.pop()
-        context.reply(", ".join([top_result.get('titleNoFormatting'),
+            top_result = results.pop(0)
+        context.reply(", ".join([unescape(top_result.get('titleNoFormatting')),
                                  top_result.get('unescapedUrl'),
                                  ]))
