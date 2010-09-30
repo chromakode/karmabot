e05938828c67fa3717ff14c43999105146ab32f2
Clark Boylan <clark.boylan@gmail.com>
2010-09-30 16:18:56 -0700

    Code clean up (PEP8), add dependency to setup.py and correct url var in setup.py.

--- karmabot/extensions/cs_schedule.py
+++ karmabot/extensions/cs_schedule.py
@@ -1,10 +1,16 @@
+"""
+An extension/facet for karmabot that will respond to queries for PSU CS course
+informmation with information gathered by crawling cs.pdx.edu for said course
+info. Depends on Beautiful Soup.
+"""
+
 import time
 import urllib
 from BeautifulSoup import BeautifulSoup
 
 from karmabot.core.client import thing
 from karmabot.core.thing import ThingFacet
-from karmabot.core.register import facet_registry, presenter_registry
+from karmabot.core.register import facet_registry
 from karmabot.core.commands.sets import CommandSet
 
 #TODO:
@@ -14,13 +20,22 @@ from karmabot.core.commands.sets import CommandSet
 
 @facet_registry.register
 class ScheduleFacet(ThingFacet):
+    """
+    Class which implements the ThingFacet interface and provides the new
+    karmabot course info reporting functionality.
+    """
     name = "course"
     commands = thing.add_child(CommandSet(name))
     URL = "http://cs.pdx.edu/schedule/termschedule?"
 
-    @commands.add(u"course {CSXXX} {TERM} {YEAR}", u"Get course information from CS website.")
-    def course1(self, context, CSXXX, TERM, YEAR):
-        SECONDS_PER_HOUR = 60 * 60
+    @commands.add(u"course {CSXXX} {TERM} {YEAR}",
+                  u"Get course information from CS website.")
+    def course(self, context, CSXXX, TERM, YEAR):
+        """
+        High level command handler for the course command. Manages
+        course cache and recrawls if timeout is exceeded.
+        """
+        SEC_PER_HOUR = 60 * 60
         sched_key = TERM + YEAR
         def to_str(item):
             if item is None:
@@ -37,31 +52,39 @@ class ScheduleFacet(ThingFacet):
                    to_str(course["Room"])
         if self.state is None:
             self.state = {}
-        state = self.state.get("cs_sched_state", {"ret_times": {}, "schedules": {}})
+        sched_state = self.state.get("cs_sched_state",
+                                     {"ret_times": {}, "schedules": {}})
 
         cur_time = time.time()
-        if cur_time - state["ret_times"].get(sched_key, 0) > SECONDS_PER_HOUR:
-            state["schedules"][sched_key] = ScheduleFacet.parse_table(ScheduleFacet.get_table(ScheduleFacet.retrieve_page(TERM, YEAR)))
-            state["ret_times"][sched_key] = cur_time
+        if cur_time - sched_state["ret_times"].get(sched_key, 0) > SEC_PER_HOUR:
+            sched_state["schedules"][sched_key] = \
+                ScheduleFacet.parse_table( \
+                    ScheduleFacet.get_table( \
+                        ScheduleFacet.retrieve_page(TERM, YEAR)
+                ))
+            sched_state["ret_times"][sched_key] = cur_time
         response = ""
-        for course in state["schedules"][sched_key]:
+        for course in sched_state["schedules"][sched_key]:
             if course["Course"] == CSXXX:
                 response = response + format_course(course) + "\n"
         context.reply(response)
-        self.state["cs_sched_state"] = state
+        self.state["cs_sched_state"] = sched_state
 
     @classmethod
     def does_attach(cls, thing):
+        """Facet does attach. Return name to signify this."""
         return thing.name == "course"
     
     @classmethod
     def retrieve_page(cls, term, year):
+        """Use urllib to grab the cs schedule html page."""
         params = urllib.urlencode({"term": term, "year": year})
         page = urllib.urlopen(cls.URL + params)
         return page.read()
 
     @classmethod
     def get_table(cls, page):
+        """Use beautiful soup to find the course schedule table in the page."""
         soup = BeautifulSoup(page)
         table = soup.find(**{"class": "userpage"}).table
         rows = table.findAll("tr")
@@ -69,6 +92,7 @@ class ScheduleFacet(ThingFacet):
 
     @classmethod
     def parse_table(cls, table):
+        """Parse the table of schedule information to make it useful."""
         def find_content(item):
             if item.small is None:
                 return None
